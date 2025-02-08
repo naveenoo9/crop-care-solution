68342dcd823c61c2e490ffb9da7623e21936e03c
naveenoo9 <50537003+naveenoo9@users.noreply.github.com>
2025-02-08 11:05:28 +0530

    Initial Project Setup

---
+++ src/main/java/com/crop_care/CropCareSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.crop_care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropCareSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropCareSolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crop_care/CropCareSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crop_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropCareSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
