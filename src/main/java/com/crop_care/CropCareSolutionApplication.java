563dc797b54a8b3ae106122b42f8bc284a5a00e6
naveenoo9 <50537003+naveenoo9@users.noreply.github.com>
2025-02-08 12:05:55 +0530

    Added Swagger UI, and updated security config

--- src/main/java/com/crop_care/CropCareSolutionApplication.java
+++ src/main/java/com/crop_care/CropCareSolutionApplication.java
@@ -3,7 +3,11 @@ package com.crop_care;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Crop Care API", version = "1.0", description = "API documentation for Crop Care Solutions"))
 public class CropCareSolutionApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/crop_care/config/SecurityConfig.java
+++ src/main/java/com/crop_care/config/SecurityConfig.java
@@ -3,13 +3,26 @@ package com.crop_care.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+
 public class SecurityConfig {
   
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    return httpSecurity.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/register", "/auth/login", "/v3/api-docs/**", "/swagger-ui/**").permitAll().anyRequest()
+                    .authenticated())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .build();
   }
 }
